7b6406b29fa498ffa7afb9cb7960c152b5322773
Rohit Kumar <159719057+rohitdotonline@users.noreply.github.com>
2024-08-06 13:36:23 +0530

    first edit, name change

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,22 +1,22 @@
 module.exports = {
-  title: "John Doe",
-  url: "https://johndoe.com",
-  icon: "/assets/images/author.jpg",
+  title: "Rohit Kumar",
+  url: "https://rohit.online",
+  // icon: "/assets/images/author.jpg",
   language: "en",
   description: "Brief description of your website or yourself",
 
   author: {
-    name: "John Doe",
-    // email: "john@doe.com",
-    url: "https://johndoe.com/about/",
-    photo: "/assets/images/author.jpg",
-    // Social media handles - remove or comment out any you don't use
-    x: "johndoe",
-    github: "https://github.com/deepakness/crispwrite",
-    // linkedin: "johndoe",
-    youtube: "johndoe",
-    // facebook: "johndoe",
-    // instagram: "johndoe",
-    // pinterest: "johndoe",
+    name: "Rohit Kumar",
+    // email: "hello@rohit.online",
+    url: "https://rohit.online/about/",
+    // photo: "/assets/images/author.jpg",
+    // // Social media handles - remove or comment out any you don't use
+    // x: "johndoe",
+    // github: "https://github.com/deepakness/crispwrite",
+    // // linkedin: "johndoe",
+    // youtube: "johndoe",
+    // // facebook: "johndoe",
+    // // instagram: "johndoe",
+    // // pinterest: "johndoe",
   }
 };
